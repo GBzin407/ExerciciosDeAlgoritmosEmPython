d32d695e216bfb06dbe143366fd307b0c69b2864
Gabriel de Oliveira Irineu <gabrielirineu998@gmail.com>
2024-12-16 15:17:23 -0300

    desafio 5

---
+++ desafios-github-fundamentals/repetindo-textos.py
@@ -0,0 +1,5 @@
+texto = input("Digite o texto que será repetido: ")
+repeticoes = int(input("Digite o número de vezes que o texto será repetido: "))
+
+resultado = texto * repeticoes
+print(f"O texto repetido é:\n{resultado}")
